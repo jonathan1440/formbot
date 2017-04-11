184a24839b805dc98b5352453fc0b6ab71d1b978
jonathan1440 <jonathan@ziesmer.us>
2017-04-11 19:49:34 -0400

    Update .py

--- .py
+++ .py
@@ -1,15 +1,77 @@
-import mechanize #sudo pip install python-mechanize
+import webbrowser
+import pyautogui
+import random
+import math
+import time 
 
-br = mechanize.Browser() #initiating a browser
+#king and queen
+#url = "https://docs.google.com/forms/d/e/1FAIpQLSenKUkczIp3jhx_sy_SFaoVHi06RKRJQrUSXhbvU-PUZCSikA/viewform?c=0&w=1"
 
-br.set_handle_robots(False) #ignore robots.txt
+#prince and princess
+url = "https://docs.google.com/a/ziesmer.us/forms/d/e/1FAIpQLSc-fTsjkPPm-IH6jshrGZXSVI19VNXs2_2S3BbJCYJD9Nz7UQ/viewform?c=0&w=1"
 
-br.addheaders = [("User-agent","Mozilla/5.0")] #our identity
+#female name spelling options
+fnames = [""]
 
-gitbot = br.open("https://docs.google.com/forms/d/e/1FAIpQLSc-fTsjkPPm-IH6jshrGZXSVI19VNXs2_2S3BbJCYJD9Nz7UQ/formResponse") #requesting the github base url
+#male name spelling options
+mnames = ["vincent rooyakkers","vincent royakkers","vincent rooyakers","vincent royakers"]
 
-br.select_form(nr=1) #the sign up form in github is in third position(search and sign in formscome before signup)
+#open page
+webbrowser.open_new(url)
+time.sleep(3)
 
-br["entry.826740370"] = "Blake Hodges"
+#select next on pg 1
+pyautogui.press('tab')
+time.sleep(1)   
 
-sign_up = br.submit()
+#click next on pg 1
+pyautogui.press('enter')
+time.sleep(3)
+
+#select pg 2 text field
+pyautogui.press('tab')
+time.sleep(1)
+
+#enter text from fnames
+pyautogui.typewrite(fnames[math.floor(random.randint(0,len(fnames)-1))])
+time.sleep(1)
+
+#selet pg 2 back
+pyautogui.press('tab')
+time.sleep(1)
+
+#select pg 2 next
+pyautogui.press('tab')
+time.sleep(1)
+
+#click pg 2 next
+pyautogui.press('enter')
+time.sleep(3)
+
+#select pg3 text field
+pyautogui.press('tab')
+time.sleep(1)
+
+#enter text from mnames
+pyautogui.typewrite(mnames[math.floor(random.randint(0,len(mnames)-1))])
+time.sleep(1)
+
+#select pg3 back
+pyautogui.press('tab')
+time.sleep(1)
+
+#select pg3 next
+pyautogui.press('tab')
+time.sleep(1)
+
+#click pg3 next
+pyautogui.press('enter')
+time.sleep(3)
+
+#select pg4 submit another response
+pyautogui.press('tab')
+time.sleep(1)
+
+#click pg4 submit another response
+pyautogui.press('enter')
+time.sleep(3)
