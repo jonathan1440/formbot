105db483e05caeb62e19489c0e9989849f50c7ef
jonathan1440 <jonathan@ziesmer.us>
2017-04-11 20:54:33 -0400

    Update .py

--- .py
+++ .py
@@ -2,7 +2,8 @@ import webbrowser
 import pyautogui
 import random
 import math
-import time 
+import time
+from datetime import datetime
 
 #king and queen
 #url = "https://docs.google.com/forms/d/e/1FAIpQLSenKUkczIp3jhx_sy_SFaoVHi06RKRJQrUSXhbvU-PUZCSikA/viewform?c=0&w=1"
@@ -23,63 +24,85 @@ numi = int(2)
 webbrowser.open_new(url)
 time.sleep(3)
 
+dt = datetime.now()
+
 while numi > 0:
-    #select next on pg 1
-    pyautogui.press('tab')
-    time.sleep(1)   
 
-    #click next on pg 1
-    pyautogui.press('enter')
-    time.sleep(3)
-
-    #select pg 2 text field
-    pyautogui.press('tab')
-    time.sleep(1)
+    print("")
+    print("iteration "+str(numi))
     
-    #enter text from fnames
-    pyautogui.typewrite(fnames[math.floor(random.randint(0,len(fnames)-1))])
-    time.sleep(1)
+    if dt.hour > 8 and dt.hour < 22:
 
-    #selet pg 2 back
-    pyautogui.press('tab')
-    time.sleep(1)
+        #select next on pg 1
+        pyautogui.press('tab')
+        #print("select next on pg 1")
+        time.sleep(3)   
 
-    #select pg 2 next
-    pyautogui.press('tab')
-    time.sleep(1)
+        #click next on pg 1
+        pyautogui.press('enter')
+        #print("click next on pg 1")
+        time.sleep(1)
 
-    #click pg 2 next
-    pyautogui.press('enter')
-    time.sleep(3)
+        #select pg 2 text field
+        pyautogui.press('tab')
+        #print("select pg 2 text field")
+        time.sleep(1)
 
-    #select pg3 text field
-    pyautogui.press('tab')
-    time.sleep(1)
+        #enter text from fnames
+        pyautogui.typewrite(fnames[math.floor(random.randint(0,len(fnames)-1))])
+        #print("enter text from fnames")
+        time.sleep(1)
 
-    #enter text from mnames
-    pyautogui.typewrite(mnames[math.floor(random.randint(0,len(mnames)-1))])
-    time.sleep(1)
+        #selet pg 2 back
+        pyautogui.press('tab')
+        #print("selet pg 2 back")
+        time.sleep(1)
 
-    #select pg3 back
-    pyautogui.press('tab')
-    time.sleep(1)
+        #select pg 2 next
+        pyautogui.press('tab')
+        #print("select pg 2 next")
+        time.sleep(1)
 
-    #select pg3 next
-    pyautogui.press('tab')
-    time.sleep(1)
+        #click pg 2 next
+        pyautogui.press('enter')
+        #print("click pg 2 next")
+        time.sleep(3)
 
-    #click pg3 next
-    pyautogui.press('enter')
-    time.sleep(3)
+        #select pg3 text field
+        pyautogui.press('tab')
+        #print("select pg3 text field")
+        time.sleep(1)
 
-    #select pg4 submit another response
-    pyautogui.press('tab')
-    time.sleep(1)
+        #enter text from mnames
+        pyautogui.typewrite(mnames[math.floor(random.randint(0,len(mnames)-1))])
+        #print("enter text from mnames")
+        time.sleep(1)
 
-    #click pg4 submit another response
-    pyautogui.press('enter')
-    time.sleep(3)
+        #select pg3 back
+        pyautogui.press('tab')
+        #print("select pg3 back")
+        time.sleep(1)
 
-    numi = numi - 1
+        #select pg3 next
+        pyautogui.press('tab')
+        #print("select pg3 next")
+        time.sleep(1)
 
-    time.sleep(random.randint(1,60) * 60)
+        #click pg3 next
+        pyautogui.press('enter')
+        #print("click pg3 next")
+        time.sleep(3)
+
+        #select pg4 submit another response
+        pyautogui.press('tab')
+        #print("select pg4 submit another response")
+        time.sleep(1)
+
+        #click pg4 submit another response
+        pyautogui.press('enter')
+        #print("click pg4 submit another response")
+        time.sleep(3)
+
+        numi = numi - 1
+
+        time.sleep(random.randint(1,60) * 60)
